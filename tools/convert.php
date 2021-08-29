85e0a41d9d41e8de7d62f801e12d6b7b0cdd207b
Necopanti <necopanti610@gmail.com>
2021-08-30 02:01:42 +0900

    [WIP] PCR＋陽性者までのExcelからjson変換処理完了

--- tools/convert.php
+++ tools/convert.php
@@ -13,17 +13,30 @@ $summaries = setSummaryJson();
 file_put_contents(__DIR__.'/../data/summaries.json', json_encode($summaries, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
 //file_put_contents(__DIR__.'/../data/patients.json', json_encode($patients, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
 
-function setSummaryJson() {
-    $data = xlsxToArray(__DIR__.'/downloads/m-covid-kensa.xlsx', 'PCR検査(件数詳細) ', 'A10:E500', 'A9:E9');
+function setSummaryJson(): array
+{
+    $data = xlsxToArray(__DIR__.'/downloads/m-covid-kensa.xlsx', 'PCR検査(件数詳細) ', 'A10:E371', 'A9:E9');
     return [
         'date' => Carbon::today()->format('Y-m-d'),
-        'data' => $data->map(function ($row) {
-            $date = Carbon::today()->year." ".str_replace(['月', '日'], ['-', ''], $row['日付']);
+        'data' => $data->filter(function ($row){
+            $today = Carbon::today();
+
+            # 行の日付整形
+            $date =  explode("日", Carbon::today()->year."-".str_replace('月', '-', $row['日付']));
+            $arrayDate = explode("-", $date[0]);
+            $rowDate = Carbon::create($arrayDate[0], $arrayDate[1], $arrayDate[2]);
+
+            # 現在日付より過去のみ対象
+            return $rowDate->lt($today);
+        })->map(function ($row) {
+            $date =  explode("日", Carbon::today()->year."-".str_replace('月', '-', $row['日付']));
+            $arrayDate = explode("-", $date[0]);
+
             return [
-                "date" => Carbon::parse($date),
+                "date" => Carbon::create($arrayDate[0], $arrayDate[1], $arrayDate[2])->format('Y-m-d'),
                 "week_count" => $row["週数"],
-                "inspection_count" => $row["検査件数"],
-                "positive_person_count" => $row["陽性数"]
+                "inspection_count" => str_replace([',', ' '], '', $row["検査件数"]),
+                "positive_person_count" => str_replace([',', ' '], '', $row["陽性数"])
             ];
         })
     ];
@@ -48,15 +61,15 @@ function xlsxToArray(string $path, string $sheet_name, string $range, $header_ra
     $spreadsheet = $reader->load($path);
     $sheet = $spreadsheet->getSheetByName($sheet_name);
     $data =  new Collection($sheet->rangeToArray($range));
-    $data = $data->map(function ($row) {
+    $data = $data->filter(function ($row){
+        return $row !== "";
+    })->map(function ($row) {
         return new Collection($row);
     });
     if ($header_range !== null) {
         $headers = xlsxToArray($path, $sheet_name, $header_range)[0];
-        // TODO check same columns length
         return $data->map(function ($row) use($headers){
             return $row->mapWithKeys(function ($cell, $idx) use($headers){
-
                 return [
                     $headers[$idx] => $cell
                 ];
